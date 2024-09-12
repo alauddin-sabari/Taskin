87d141a7de5e563bcea8e28314918765597269a8
taskin1124 <rahtaskin@gmail.com>
2024-09-12 16:44:51 +0100

    readding friend food because of error

---
+++ L-12 set changes/friend_food.py
@@ -0,0 +1,7 @@
+friends_food={'pizza','chips','fish','egg','fried chicken','soup'}
+def intersect (your_food,friends_food):
+    some=your_food.intersection(friends_food)
+    print (some)
+    return some
+
+inter=intersect(your_food,friends_food)
